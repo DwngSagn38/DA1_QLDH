c8f6123a969b3881d53e6104dff53560d17d3391
KieuTrinh3007 <phamkieutrinh300720044@gmail.com>
2023-11-20 20:16:48 +0700

    nn

--- app/src/main/java/com/example/da1_qldh_yuii/Welcome.java
+++ app/src/main/java/com/example/da1_qldh_yuii/Welcome.java
@@ -11,5 +11,7 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         // hihi
+
+        // abccccc
     }
 }
