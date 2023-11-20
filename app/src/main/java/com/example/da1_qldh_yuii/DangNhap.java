6f3a62155f717881e4e3af0addfa86855a3244eb
KieuTrinh3007 <phamkieutrinh300720044@gmail.com>
2023-11-20 15:22:55 +0700

    dangnhap

--- app/src/main/java/com/example/da1_qldh_yuii/DangNhap.java
+++ app/src/main/java/com/example/da1_qldh_yuii/DangNhap.java
@@ -1,10 +1,13 @@
 package com.example.da1_qldh_yuii;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,6 +17,7 @@ import android.widget.Toast;
 
 import com.example.da1_qldh_yuii.dao.ThanhVienDAO;
 import com.example.da1_qldh_yuii.fragment.fragment_trangchu;
+import com.example.da1_qldh_yuii.model.ThanhVien;
 
 public class DangNhap extends AppCompatActivity {
 
@@ -26,6 +30,10 @@ public class DangNhap extends AppCompatActivity {
     ThanhVienDAO tvDAO;
 
     String strUser,strPass;
+
+    EditText edUser,edTen,edsdt,edPass,edRePass;
+
+    Button btnLuu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,10 +66,50 @@ public class DangNhap extends AppCompatActivity {
             }
         });
 
+        tvDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
 
     }
 
+    public void openDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dl_dangky,null);
+        builder.setView(view); // gan view vao hop thoai
+        Dialog dialog = builder.create();
+        dialog.show();
 
+        // anh xa
+        edUser = dialog.findViewById(R.id.edUser);
+        edTen = dialog.findViewById(R.id.edTen);
+        edsdt = dialog.findViewById(R.id.edsdt);
+        edPass = dialog.findViewById(R.id.edPass);
+        edRePass = dialog.findViewById(R.id.edRePass);
+        btnLuu = dialog.findViewById(R.id.btnLuu);
+        //
+        btnLuu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = edUser.getText().toString();
+                String hoten = edTen.getText().toString();
+                String sdt = edsdt.getText().toString();
+                String pass = edPass.getText().toString();
+                String confirm = edRePass.getText().toString();
+                ThanhVien tv = new ThanhVien(user,hoten,sdt,pass,2);
+                if (user.trim().isEmpty() || hoten.trim().isEmpty() || sdt.trim().isEmpty() || pass.trim().isEmpty() || confirm.trim().isEmpty()) {
+                    Toast.makeText(DangNhap.this, "Không được để trống thông tin", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+    }
 
     public void rememberUser(String u, String p, boolean status) {
         SharedPreferences pref = getSharedPreferences("USER_FILE", MODE_PRIVATE);
